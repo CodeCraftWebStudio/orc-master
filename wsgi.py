623bd21cc075099b289ec91e4967305255dcd751
Solomon Afolabi <techprovix@gmail.com>
2026-03-08 20:18:00 +0100

    Fix database initialization context

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 from backend.Router import app
 from backend.models.model_base.general_base import create_db
 
-create_db()
+with app.app_context():
+    create_db()
 
 if __name__ == "__main__":
     import os
