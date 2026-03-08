6420fa8a04b70467248daebed88d50d76e30a878
Solomon Afolabi <techprovix@gmail.com>
2026-03-08 16:13:40 +0100

    Fix CORS and database config

--- backend/Database.py
+++ backend/Database.py
@@ -2,8 +2,13 @@ from flask import Flask, jsonify, redirect, request
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config["JSON_SORT_KEYS"] = False
 CORS(
     app,
     supports_credentials=True,
-    origins=["http://localhost:*", "http://127.0.0.1:*"]
+    origins=[
+        "http://localhost:*",
+        "http://127.0.0.1:*",
+        "https://boisterous-alfajores-6bcd79.netlify.app"
+    ]
 )
--- backend/models/model_base/general_base.py
+++ backend/models/model_base/general_base.py
@@ -2,7 +2,7 @@ from ...Database import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///obafemiawolowouniversityinernationalschoolredcrosssocietyweb.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "connect_args": {"check_same_thread": False}
@@ -11,13 +11,14 @@ app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class RoleType:
     GUEST = "guest"
     MEMBER = "member"
     NON_EDITOR_EXEC = "non_editor_exec"
     EDITING_EXEC = "editing_exec"
     ADMIN = "admin"
- 
+
 
 def create_db():
     with app.app_context():
