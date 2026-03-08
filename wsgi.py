72afb5a73965eae0768098116d3d3a34602446eb
Solomon Afolabi <techprovix@gmail.com>
2026-03-08 09:21:49 +0100

    Fix Gunicorn app loading

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,11 @@ import os
 from backend.Router import app, socketio
 from backend.models.model_base.general_base import create_db
 
+create_db()
+
+
+application = app
+
 if __name__ == "__main__":
-    create_db()
     port = int(os.environ.get("PORT", 10000))
     socketio.run(app, host="0.0.0.0", port=port)
