87cf6ade38ee048185ad204eb02b3dcb8cacc38f
Solomon Afolabi <techprovix@gmail.com>
2026-03-08 09:10:51 +0100

    Fix database initialization for deployment

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
+import os
 from backend.Router import app, socketio
 from backend.models.model_base.general_base import create_db
 
-create_db()
-
 if __name__ == "__main__":
-    socketio.run(app)
+    create_db()
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host="0.0.0.0", port=port)
