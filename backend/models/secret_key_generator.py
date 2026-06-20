f9da14b64dca7a8894ddc7675d21ba044bd2431f
CodeCraftWebStudio <studiocodecraft099@gmail.com>
2026-06-20 21:27:25 +0100

    Update secret_key_generator.py

--- backend/models/secret_key_generator.py
+++ backend/models/secret_key_generator.py
@@ -1,5 +1,6 @@
+import os
 from ..local_storage import localStorage
-
+from dotenv import load_dotenv
 
 def dehasher(string):
     return string
@@ -8,12 +9,12 @@ def dehasher(string):
 def hasher(string):
     return string
 
-
+load_dotenv()
 LOCAL_STORAGE = localStorage
 orc = 'ORC'
 orc_key = 'Xh84PtDmGYycPnlLCyPLBVYBzpUZbPJdA2t2zxJhZKQ='
 api_key = 'GEMINI_API_KEY'
-api = 'AIzaSyDce1_NgK4GA0YeT--tZ0QWCk2-AnKRRnM'
+api = os.getenv('GEMINI_API_KEY')
 LOCAL_STORAGE.setItem(api_key, api)
 LOCAL_STORAGE.setItem(orc, orc_key)
 print(LOCAL_STORAGE.getItem(orc))
